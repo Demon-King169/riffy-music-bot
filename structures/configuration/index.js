16961af9fa4077760096793fedb9c6cadf52c64d
Demon-King169 <161844189+Demon-King169@users.noreply.github.com>
2024-03-16 16:37:23 +0530

    Update index.js

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -1,16 +1,16 @@
 module.exports = {
-    client_token: "",
-    client_id: "",
-    client_prefix: "!",
-    mongodb_url: "", //optional
-    developers: ["786504767358238720"],
-    sharding: false,
-    database: false,
+    client_token: "MTIxNjMxOTExOTY1ODU4MjEyNg.Gp9EF5.cLiAcjlFMCXJExsKrWvrEG13Ow-suyjLrwrg_s",
+    client_id: "1216319119658582126",
+    client_prefix: ".",
+    mongodb_url: "mongodb+srv://demon:discord@demon.gcqnpcd.mongodb.net/?retryWrites=true&w=majority", //optional
+    developers: ["1207510451005427783"],
+    sharding: true,
+    database: true,
     nodes: [
         {
-            host: "localhost",
-            port: 2333,
-            password: "youshallnotpass",
+            host: "node1.paradisehost.xyz",
+            port: 27236,
+            password: "lava",
             secure: false
         }
     ]
